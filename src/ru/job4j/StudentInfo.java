22c26fbeb465062dbc2840535b9e54fd93ee1f1d
Денис <DEmmaxes1988@mail.ru>
2020-05-18 22:34:30 +0500

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Denis Davletkildin");
+        System.out.println("14.09.1988");
+    }
+}
