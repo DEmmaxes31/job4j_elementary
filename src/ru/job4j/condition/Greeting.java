7191e9a5de25b8d9ab4e569acef40614aec88673
Денис <DEmmaxes1988@mail.ru>
2020-05-20 23:58:27 +0500

    1. Модульные тесты.[159435#291722]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String  idea = "I like Java!";
-        System.out.println(idea + "But I am a newbie.");
+        String  idea1 = "But I am a newbie.";
         int year = 2020;
-        System.out.println(year);
+        System.out.println(idea + idea1 + year);
     }
 }
